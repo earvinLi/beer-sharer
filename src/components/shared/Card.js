af37fa2735d4b4e8351f3a53ed5c2331b8153b59
Earvin Li <earvin.tli@gmail.com>
2018-11-20 10:03:22 +0800

    [App] Create a shared Card component

---
+++ src/components/shared/Card.js
@@ -0,0 +1,28 @@
+// External Dependencies
+import React from 'react';
+import { View } from 'react-native';
+
+// Component Definition
+const Card = ({ children }) => <View style={styles.container}>{children}</View>;
+
+// May change to shorthands by css-to-react-native in the future
+const styles = {
+  container: {
+    borderBottomWidth: 0,
+    borderColor: '#ddd',
+    borderRadius: 2,
+    borderWidth: 1,
+    elevation: 1,
+    marginLeft: 5,
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+  },
+};
+
+export default Card;
