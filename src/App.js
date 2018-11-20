65b95ff5dbf2dcded74e23b2d9007868168448e4
Earvin Li <earvin.tli@gmail.com>
2018-11-20 08:27:16 +0800

    [App] Modify App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to Beer Sharer</Text>
+    </View>
+  );
+};
+
+export default App;
