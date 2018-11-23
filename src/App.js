96f636f80dcdf880a107927aa6335782bd24d4b7
Earvin Li <earvin.tli@gmail.com>
2018-11-23 09:03:29 +0800

    [App] Fix importing firebase API key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 import React, { Component } from 'react';
 
 // Internal Dependencies
-import fireBaseInitConfig from './OuthConfig';
+import { fireBaseInitConfig } from './OuthConfig';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
