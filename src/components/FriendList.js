d7e7aa8f19694542b8e5a1e67b83af5611314952
Earvin Li <earvin.tli@gmail.com>
2018-11-25 18:39:05 +0800

    [Friend List] When talking about beer, who will always occur to your mind?

---
+++ src/components/FriendList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+class FriendList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Jeffrey</Text>
+        <Text>Hyde</Text>
+        <Text>Mark</Text>
+      </View>
+    );
+  }
+}
+
+export default FriendList;
