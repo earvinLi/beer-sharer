5c91c89a00b4ff6853db8cbf08fdc5e07c70be7a
Earvin Li <earvin.tli@gmail.com>
2019-01-30 12:48:15 +0800

    [Sign Out] Restructure signOutUser

--- src/Auth/actions/SignOutAction.js
+++ src/Auth/actions/SignOutAction.js
@@ -3,20 +3,20 @@ import firebase from 'firebase';
 import { AsyncStorage } from 'react-native';
 
 // Internal Dependencies
+import { createActionCreator } from '../../App/RootUtilities';
 import { SIGN_OUT_USER_FAIL } from '../../App/ActionTypes';
+
+const signOutUserFail = createActionCreator(SIGN_OUT_USER_FAIL, 'signOutFailError');
 
 export const clearCache = () => {};
 
-export const signOutUser = navigation => async (dispatch) => {
-  await Promise.all([
-    firebase.auth().signOut(),
-    AsyncStorage.removeItem('accountId'),
-  ]).catch((signOutError) => {
-    dispatch({
-      type: SIGN_OUT_USER_FAIL,
-      payload: signOutError,
-    });
-  });
+export const signOutUser = async (navigation) => {
+  // TODO: Actions must be plain objects?
+  await firebase.auth().signOut()
+    .catch(signOutFailError => signOutUserFail(signOutFailError));
+
+  // TODO: Unhandled promise rejection?
+  await AsyncStorage.removeItem('accountId');
 
   navigation.navigate('Auth');
 };
