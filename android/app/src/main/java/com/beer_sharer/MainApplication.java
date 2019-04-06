5bf7fcdbf40cd70dcbdc40c264a984c90821f926
Earvin Li <earvin.tli@gmail.com>
2019-04-06 14:28:31 +0800

    [App] Install and link react-native-image-picker

--- android/app/src/main/java/com/beer_sharer/MainApplication.java
+++ android/app/src/main/java/com/beer_sharer/MainApplication.java
@@ -3,6 +3,7 @@ package com.beer_sharer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNAWSCognitoPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
