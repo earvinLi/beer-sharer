a1d201b6dde0c4ada341e597550e427bb6a1030f
Earvin Li <earvin.tli@gmail.com>
2019-04-10 17:13:01 +0800

    [App] Install and link react-native-fetch-blob

--- android/app/src/main/java/com/beer_sharer/MainApplication.java
+++ android/app/src/main/java/com/beer_sharer/MainApplication.java
@@ -3,6 +3,7 @@ package com.beer_sharer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new ImagePickerPackage(),
             new RNAWSCognitoPackage(),
             new VectorIconsPackage(),
