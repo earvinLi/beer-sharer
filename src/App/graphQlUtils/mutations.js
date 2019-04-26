eaf1a382cb37433aa5503ecd93e9c340f05f9816
Earvin Li <earvin.tli@gmail.com>
2019-04-26 14:17:45 +0800

    [App] Change to use BeerSharerUserAPI

---
+++ src/App/graphQlUtils/mutations.js
@@ -0,0 +1,11 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const addUser = `mutation AddUser($id: ID!, $username: String, $email: String) {
+  addUser(id: $id, username: $username, email: $email) {
+    id
+    username
+    email
+  }
+}
+`;
